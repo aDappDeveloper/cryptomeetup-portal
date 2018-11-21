8055a19e98e4c395bc29c1041b9d0d1df996c507
Adappdeveloper <1dappdeveloper@gmail.com>
2018-11-21 23:40:16 +0800

    upload zh_tw

---
+++ src/i18n/zh_tw.js
@@ -0,0 +1,143 @@
+/* eslint-disable */
+// const ABOUT_CONTENT = require('raw-loader!./about_zh.html');
+import ABOUT_CONTENT from './about_zh_tw.html';
+
+export default {
+    switch_lang : ' 切換語言 ',
+
+    Globe: ' 地球 ',
+    
+    Map: ' 地圖 ',
+    
+      token_view : ' 通證 ',
+    
+      about_view : ' 關於 ',
+    
+    login: ' 登錄 ',
+    
+    logout: ' 登出 ',
+    
+    back: ' 後退 ',
+    
+      filter_country_or_region : ' 選擇國家或地區 ',
+    
+      my_EOS : ' 我的 EOS',
+    
+      my_CMU : ' 我的 CMU',
+    
+      my_staked : ' 我的抵押 ',
+    
+      total_staked : ' 合計抵押 ',
+    
+      my_dividend : ' 我的分紅 ',
+    
+      total_dividend : ' 合計分紅 ',
+    
+      contract_supply : ' 合約發行量 ',
+    
+      contract_balance : ' 合約儲備金 ',
+    
+      contract_price : ' 合約幣價 ',
+    
+     
+    
+      payout_pool_tab : ' 紅利池 ',
+    
+      my_assets_tab : ' 我的資產 ',
+    
+      stake_tab : ' 抵押 ',
+    
+      bancor_trade_tab : ' Bancor 交易 ',
+    
+     
+    
+      stake_btn : ' 抵押 ',
+    
+      unstake_btn : ' 取消抵押 ',
+    
+      buy_btn : ' 購買 ',
+    
+      sell_btn : ' 賣出 ',
+    
+      claim_btn : ' 領取分紅 ',
+    
+     
+    
+      last_buyer : ' 最後的買家 ',
+    
+      count_down : ' 剩餘時間 ',
+    
+      prize_pool : ' 獎池 ',
+    
+    ABOUT_CONTENT,
+    
+     
+    
+      cmu_creator : ' 由 Cryptomeetup （幣聚）團隊傾力打造 ',
+    
+      powered_by : 'Powered By ',
+    
+     
+    
+      stake_number_alert : ' 你要抵押多少 CMU ？ ',
+    
+      stake_successful_alert : 'CMU 代幣抵押成功 ',
+    
+      stake_pay_attention_alert : ' 稍後留意 My Staked',
+    
+      stake_confirmed_btn : ' 確認 ',
+    
+      unstake_alert : ' 你要撤銷抵押多少 CMU ？ ',
+    
+      unstake_success : ' 撤銷抵押成功 ',
+    
+      claim_success : ' 提取分紅成功 ',
+    
+      wait_alert : ' 請耐心等待 ',
+    
+      claim_fail : ' 提取失敗 ',
+    
+      buy_cmu_alert : ' 你要購買多少 EOS 等值的 CMU ？ ',
+    
+      buy_cmu_success_alert : 'CMU 代幣購買成功 ',
+    
+      after_buy_cmu_alert : ' 稍後留意 CMU 餘額變動 ',
+    
+      sell_cmu_alert : ' 你要賣出多少 CMU ？ ',
+    
+    ok: ' 好的 ',
+    
+      sell_cmu_success_alert : 'CMU 成功賣出 ',
+    
+      after_sell_cmu_alert : ' 稍後留意 EOS 餘額變動 ',
+    
+      scatter_login_fail : 'Scatter 登入失敗：用戶的 Scatter 錢包已被鎖定，在進行下一步之前用戶已經被通知 ',
+    
+      scatter_login_success : ' 成功使用 Scatter 錢包登錄 ',
+    
+     
+    
+      buy_land_withApp_success : ' 轉賬成功， 30 秒內自動刷新數據，即可確認你是否為新地主。 一切以區塊鏈上交易記錄為準，購買地皮失敗則退款。 ',
+    
+      buy_land_success_alert : ' 成功購買 ',
+    
+      buy_land_success_msg : ' 轉賬已提交到區塊鏈， 30 秒後自動刷新數據，即可確認是否購買成功。 ',
+    
+      buy_land_success_comfm : ' 好的 ',
+    
+      token_FTextBubble :' 如果您的股息餘額大於零，您可以申請股息。 ',
+    
+      app_FTextBubble :' 將 CMU 交換到 EOS',
+    
+     
+    
+      input_title : ' 請輸入地標名稱 ',
+    
+      input_description : ' 請輸入地標描述 ',
+    
+      input_nickname : ' 暱稱 ',
+    
+      upload_photo : ' 上傳地標圖片 ',
+    
+      confirm_update : ' 確認 ',
+};
